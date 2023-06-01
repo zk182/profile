fa794dae84dffa12ff0d0b8e498c0d063b527a89
Nicolas Battaglia <nicobattmdq@gmail.com>
2023-06-01 19:39:29 -0300

    Add simple footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+
+
+function Footer() {
+    return (
+        <footer>          
+            © {new Date().getFullYear()} Nicolas Battaglia. All rights reserved. 
+        </footer>
+    )
+}
+
+export default Footer;
