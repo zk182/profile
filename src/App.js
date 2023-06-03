baa1974e12c6198e08f6b6141e20670059c604c5
Nicolas Battaglia <nicobattmdq@gmail.com>
2023-06-02 22:53:28 -0300

    Add avatar and work js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './style/App.css';
 import Footer from './components/Footer';
+import { Avatar } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Avatar/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/components/Work.js
@@ -0,0 +1,16 @@
+
+
+function Work() {
+
+
+}
+
+
+
+function Experience (){
+    return (
+        <Work></Work>
+    )
+}
+
+export default Experience;
