50e70ec4c3f9dcee718d9654a480c3fab34e0cdd
Nicolas Battaglia <nicobattmdq@gmail.com>
2023-06-02 23:52:32 -0300

    Typescript added

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './style/App.css';
 import Footer from './components/Footer';
@@ -6,11 +7,11 @@ import { Avatar } from '@mui/material';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
       <Avatar/>
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
